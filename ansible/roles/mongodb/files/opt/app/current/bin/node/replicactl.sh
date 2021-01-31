4e3323b761d8a44a72bbd6e623b0aa083368be6c
Yi jing <djangoyi@yunify.com>
2021-01-31 10:20:13 +0800

    bug-fix: can not extrack tgz file to target path

--- ansible/roles/mongodb/files/opt/app/current/bin/node/replicactl.sh
+++ ansible/roles/mongodb/files/opt/app/current/bin/node/replicactl.sh
@@ -1,5 +1,12 @@
 # sourced by /opt/app/current/bin/ctl.sh
+init() {
+  if [ ! -d /data/db ];then
+    mkdir /data/db
+    chown mongod:svc /data/db
+  fi
+  _init
+}
 
 start() {
-    echo 'hello world!'
+  _start
 }
