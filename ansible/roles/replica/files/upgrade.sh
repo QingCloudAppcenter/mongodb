b9e55e097d2b70574cec36927473fcfb405b2e6c
Yi jing <djangoyi@yunify.com>
2021-03-22 13:59:05 +0800

    code review runMongoCmd,runMongoCmdEx => runMongoCmd isMaster,isMasterEx=>isMaster

--- ansible/roles/replica/files/upgrade.sh
+++ ansible/roles/replica/files/upgrade.sh
@@ -49,28 +49,14 @@ getMongoPort() {
   awk '$1=="port:" {print $2}' /etc/mongod.conf
 }
 
-runMongoCmd() {
-  local passwd="$(cat /data/pitrix.pwd)"
-  local port=$(getMongoPort)
-  local uri=mongodb://qc_master:$passwd@127.0.0.1:$port/admin
-  if [ "$1" = "--local" ]; then
-    shift
-  else
-    uri=$uri?replicaSet=foobar
-  fi
-  timeout --preserve-status 3 /opt/mongodb/bin/mongo --quiet $uri --eval "$@"
-}
-
 readonly EC_NOT_READY=128
 
+# checkFullyStarted
+# desc: check if this node's stateStr is primary or secondary
 checkFullyStarted() {
-  local myIp=$(hostname -I | xargs)
+  local myIp=$(hostname -I | cut -d' ' -f1)
   local port=$(getMongoPort)
   runMongoCmd "rs.status().members.filter(m => m.name == '$myIp:$port' && /(PRIMARY|SECONDARY)/.test(m.stateStr)).length == 1 || quit($EC_NOT_READY)"
-}
-
-isMaster() {
-  runMongoCmd --local "db.isMaster().ismaster == true || quit(1)"
 }
 
 readonly oldMongoVersion=3.4.5
@@ -95,24 +81,20 @@ rollback() {
   ln -snf $oldMongoVersion/bin /opt/mongodb/bin
 }
 
-# runMongoCmdEx
+# runMongoCmd
 # desc run mongo shell
 # $1: script string
-# $2/$3: username/passwd (option)
-# $4: ip (option)
-runMongoCmdEx() {
-  if [ $# -ne 1 ] && [ $# -ne 3 ] && [ $# -ne 4 ]; then return 1; fi
+# $2: ip (option)
+runMongoCmd() {
+  local passwd="$(cat /data/pitrix.pwd)"
+  local port=$(getMongoPort)
 
-  local cmd="/opt/mongodb/bin/mongo --quiet --port $(getMongoPort)"
+  local cmd="/opt/mongodb/bin/mongo --quiet --port $port --authenticationDatabase admin --username qc_master --password $passwd"
   local jsstr="$1"
 
   shift
-  if [ $# -gt 0 ]; then
-    cmd="$cmd --authenticationDatabase admin --username $1 --password $2"
-    shift 2
-    if [ $# -ne 0 ]; then
-      cmd="$cmd --host $1"
-    fi
+  if [ $# -gt 0 ] && [ -n "$1" ]; then
+    cmd="$cmd --host $1"
   fi
 
   timeout --preserve-status 5 echo "$jsstr" | $cmd
@@ -126,8 +108,11 @@ getNodeId() {
   echo $(echo $1 | cut -d'|' -f1)
 }
 
-isMasterEx() {
-  runMongoCmdEx "db.isMaster().ismaster == true || quit(1)" "qc_master" "$(cat /data/pitrix.pwd)" "$1" > /dev/null
+# isMaster
+# desc: judge if the node is a primary node
+# $1: node's ip (option)
+isMaster() {
+  runMongoCmd "db.isMaster().ismaster == true || quit(1)" "$1" > /dev/null
 }
 
 # getOrder
@@ -148,7 +133,7 @@ getOrder() {
   local res=''
   local mas=''
   for((i=0;i<${#tmpstr[@]};i++)); do
-    if isMasterEx $(getIp ${tmpstr[i]}); then
+    if isMaster $(getIp ${tmpstr[i]}); then
       mas=$(getNodeId ${tmpstr[i]})
     else
       res="$res$(getNodeId ${tmpstr[i]}),"
@@ -167,7 +152,7 @@ ver=db.version()
 if (ver.indexOf("$1") !=0 ) { quit(1) }
 EOF
 )
-  runMongoCmdEx "$jsstr" "qc_master" "$(cat /data/pitrix.pwd)"
+  runMongoCmd "$jsstr"
 }
 
 # isFCVOk
@@ -175,7 +160,7 @@ EOF
 # input: $1 desire version number, etc 3.6(not 3.6.8)
 isFCVOk() {
   local jsstr="db.adminCommand({getParameter:1,featureCompatibilityVersion:1})"
-  local res=$(runMongoCmdEx "$jsstr" "qc_master" "$(cat /data/pitrix.pwd)")
+  local res=$(runMongoCmd "$jsstr")
   res=$(echo "$res" | sed -n '/[vV]ersion/p' |  grep -o '[[:digit:].]\{2,\}')
   test "$1" = "$res"
 }
@@ -193,7 +178,7 @@ if (members.filter(m => /(1|2)/.test(m.state)).length != $nodecnt) {
 EOF
 )
 
-  runMongoCmdEx "$jsstr" "qc_master" "$(cat /data/pitrix.pwd)"
+  runMongoCmd "$jsstr"
 }
 
 readonly ERROR_UPGRADE_BADVERSION=33
@@ -209,7 +194,7 @@ precheck() {
 # desc: primary node does step down first
 # input: $1-seconds during which the node can't be primary again
 doStepDown() {
-  if runMongoCmdEx "rs.stepDown($1)" "qc_master" "$(cat /data/pitrix.pwd)"; then
+  if runMongoCmd "rs.stepDown($1)"; then
     # need check error status
     log "need check error status"
     retrun 1
@@ -222,7 +207,7 @@ doStepDown() {
 # isNewPrimaryOk
 # desc: judge if new primary is elected
 isNewPrimaryOk() {
-  local res=$(runMongoCmdEx "rs.isMaster()" "qc_master" "$(cat /data/pitrix.pwd)")
+  local res=$(runMongoCmd "rs.isMaster()")
   local primary=$(echo "$res" | sed -n '/"primary"/p' | grep -o '[[:digit:]][[:digit:].:]\+')
   local me=$(echo "$res" | sed -n '/"me"/p' | grep -o '[[:digit:]][[:digit:].:]\+')
   test "$primary" != "$me"
@@ -266,10 +251,10 @@ doRollback() {
 }
 
 # showFCV
-# desc: display feature compatibility version according
+# desc: display feature compatibility version on web console
 showFCV() {
   local jsstr="db.adminCommand({getParameter:1,featureCompatibilityVersion:1})"
-  local res=$(runMongoCmdEx "$jsstr" "qc_master" "$(cat /data/pitrix.pwd)")
+  local res=$(runMongoCmd "$jsstr")
   res=$(echo "$res" | sed -n '/[vV]ersion/p' |  grep -o '[[:digit:].]\{2,\}')
   local tmpstr=$(cat <<EOF
 {
@@ -289,14 +274,14 @@ changeFCV() {
   if ! isMaster; then return; fi
 
   local jsstr="db.adminCommand({getParameter:1,featureCompatibilityVersion:1})"
-  local res=$(runMongoCmdEx "$jsstr" "qc_master" "$(cat /data/pitrix.pwd)")
+  local res=$(runMongoCmd "$jsstr")
   res=$(echo "$res" | sed -n '/[vV]ersion/p' |  grep -o '[[:digit:].]\{2,\}')
   if [ "$res" = "$1" ]; then return; fi
 
   if ! isReplicasSetStatusOk; then return; fi
 
   jsstr="db.adminCommand({setFeatureCompatibilityVersion:\"$1\"})"
-  runMongoCmdEx "$jsstr" "qc_master" "$(cat /data/pitrix.pwd)"
+  runMongoCmd "$jsstr"
 }
 
 main() {
